b3f051830e4daaeec741786ccfaa105a4246cdd7
remuring9999 <mooneda9999@gmail.com>
2024-02-06 12:36:20 +0900

    모듈 누락 xml2js

--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,9 @@ import YoutubeClient from "./YoutubeClient";
 
 (async () => {
   const client = new YoutubeClient();
-  await client.getVideoInfo("rDFUl2mHIW4");
-  const caption = await client.parseTimedText("ko");
-  const filtered = await client.mergeTimedText(caption.json);
+  await client.getVideoInfo("fgSXAKsq-Vo");
+  const caption = await client.parseTimedText("ja");
+  // const filtered = await client.mergeTimedText(caption.json);
   console.log(caption?.json);
-  console.log(filtered?.json);
+  // console.log(filtered?.json);
 })();
