26d6e18e14d82e988ddc629204ce87c45efa3923
レムリン <80942588+remuring9999@users.noreply.github.com>
2024-08-20 21:43:24 +0900

    update: 오래된 브라우저 어쩌구 대응

--- src/YoutubeClient.ts
+++ src/YoutubeClient.ts
@@ -14,7 +14,7 @@ class YoutubeClient {
     this.client = axios.create({
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
       },
     });
   }
